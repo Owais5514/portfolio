07694d8a4a0333297f278d7b421b93bf0f2b595b
Shad Ahmed Raiyan <101061268+Owais5514@users.noreply.github.com>
2024-11-17 13:22:19 +0000

    Added a blog section. Also added two test blogs. Some changes to the home page to include blog.

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -19,19 +19,19 @@ export default function Home() {
       <FadeIn.Item>
         <p>
           Hello! My Name is Shad Ahmed Raiyan. I am a University Student currenly learning a few things about Github!
-          This webpage is still under construction. I am still learning how to use Github and how to make a webpage. I am
-          going to make this my go-to online CV and portfolio. I am going to add all my projects and experiences here. I 
-          am also going to add a blog section where I will write about my experiences and thoughts. So Stay Tuned!
+          This webpage is still under construction. 
+        </p>
+        <p>
+          I am still learning how to use Github and how to make a webpage. I am going to make this my go-to online CV 
+          and portfolio. I am going to add all my projects and experiences here. I am also going to add a blog section 
+          where I will write about my experiences and thoughts. So Stay Tuned!
         </p>
       </FadeIn.Item>
-      {/* <FadeIn.Item>
-        <Posts category="guides" />
-      </FadeIn.Item>
-      <FadeIn.Item>
-        <Posts category="examples" />
-      </FadeIn.Item> */}
       <FadeIn.Item>
         <Posts category="information" />
+      </FadeIn.Item>
+      <FadeIn.Item>
+        <Posts category="blog" />
       </FadeIn.Item>
       <Spacer />
       <FadeIn.Item>
