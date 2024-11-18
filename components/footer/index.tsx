e6d713d359cb376152dfab5a6761e4336119544f
Shad Ahmed Raiyan <101061268+Owais5514@users.noreply.github.com>
2024-11-18 03:24:04 +0000

    added some footnotes to homepage

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <div className="flex w-full items-center justify-between border-border border-t pt-2">
       <div className="px-[2px] text-muted text-small">
-        Built with <Link href="https://nextjs.org/" text="Next.js" underline />
+        Built by Shad Ahmed Raiyan using <Link href="https://nextjs.org/" text="Next.js" underline />
+        and template from <Link href="https://next-sylph-portfolio.vercel.app/" text="Sylph" underline />
       </div>
       <div className="text-muted text-small">
         <AppThemeSwitcher />
